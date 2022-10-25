9d0ed7565535efede67a46fcb1850d239e230bf2
RubensPS <rubens_p_souza@hotmail.com>
2022-10-25 16:10:27 -0300

    feature: Creating Telephone class

---
+++ src/main/java/com/school/mjvpetshop/model/customer/Telephone.java
@@ -0,0 +1,8 @@
+package com.school.mjvpetshop.model.customer;
+
+public class Telephone {
+    private PhoneType type;
+    private Integer ddd;
+    private Integer number;
+    private Long customerId;
+}
