2f730c6446ba9ebb80ff29bc1484500b7a748133
RubensPS <rubens_p_souza@hotmail.com>
2022-10-25 16:36:36 -0300

    feature: Creating CartEntity class

---
+++ src/main/java/com/school/mjvpetshop/model/cart/CartEntity.java
@@ -0,0 +1,10 @@
+package com.school.mjvpetshop.model.cart;
+
+import java.math.BigDecimal;
+
+public class CartEntity {
+    private Long id;
+    private Set<CartItem>;
+    private BigDecimal totalShopValue;
+
+}
