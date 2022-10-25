51a4e0ae624adbed2fbf83121525337b1ad5c9e3
RubensPS <rubens_p_souza@hotmail.com>
2022-10-25 15:30:49 -0300

    feature: Initial commit. Creating spring project.

---
+++ src/main/java/com/school/mjvpetshop/MjvPetShopApplication.java
@@ -0,0 +1,13 @@
+package com.school.mjvpetshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MjvPetShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MjvPetShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/school/mjvpetshop/MjvPetShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.school.mjvpetshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MjvPetShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
