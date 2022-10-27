1869d27de6bd72c8e1a5daa7da6f3b17771b25c8
RubensPS <rubens_p_souza@hotmail.com>
2022-10-26 23:47:30 -0300

    feat: implemented static method for customer entity to response conversion

--- src/main/java/com/school/mjvpetshop/dtoConversion/CustomerDtoConversion.java
+++ src/main/java/com/school/mjvpetshop/dtoConversion/CustomerDtoConversion.java
@@ -2,11 +2,16 @@ package com.school.mjvpetshop.dtoConversion;
 
 import com.school.mjvpetshop.model.customer.CustomerEntity;
 import com.school.mjvpetshop.model.customer.CustomerRequest;
+import com.school.mjvpetshop.model.customer.CustomerResponse;
 
 public class CustomerDtoConversion {
 
     public static CustomerEntity requestToEntity(CustomerRequest request) {
         return new CustomerEntity(request);
+    }
+
+    public static CustomerResponse entityToResponse(CustomerEntity entity) {
+        return new CustomerResponse(entity);
     }
 
 
