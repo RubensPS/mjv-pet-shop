1bbf493507963c6fd35fdb915133b45bc89eb38c
RubensPS <rubens_p_souza@hotmail.com>
2022-10-26 22:11:55 -0300

    feat: creating CustomerController

---
+++ src/main/java/com/school/mjvpetshop/controller/CustomerController.java
@@ -0,0 +1,11 @@
+package com.school.mjvpetshop.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/customers")
+public class CustomerController {
+    
+
+}
