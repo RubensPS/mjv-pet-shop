8215a500027985f658f2bb5371b15f7608870200
RubensPS <rubens_p_souza@hotmail.com>
2022-10-27 14:21:35 -0300

    feat: implementing GET customer by id service

--- src/main/java/com/school/mjvpetshop/service/CustomerService.java
+++ src/main/java/com/school/mjvpetshop/service/CustomerService.java
@@ -1,6 +1,7 @@
 package com.school.mjvpetshop.service;
 
 import com.school.mjvpetshop.dtoConversion.CustomerDtoConversion;
+import com.school.mjvpetshop.exception.CustomerNotFoundException;
 import com.school.mjvpetshop.model.customer.CustomerEntity;
 import com.school.mjvpetshop.model.customer.CustomerRequest;
 import com.school.mjvpetshop.model.customer.CustomerResponse;
@@ -17,6 +18,11 @@ public class CustomerService {
     public CustomerResponse saveNewCustomer(CustomerRequest request) {
         CustomerEntity entity = CustomerDtoConversion.requestToEntity(request);
         return CustomerDtoConversion.entityToResponse(customerRepository.save(entity));
+    }
+
+    public CustomerResponse findCustomerById(Long id) {
+        CustomerEntity entity = customerRepository.findById(id).orElseThrow(() -> new CustomerNotFoundException("A customer with the provided ID doesn't exist in the database."));
+        return CustomerDtoConversion.entityToResponse(entity);
     }
 
 }
