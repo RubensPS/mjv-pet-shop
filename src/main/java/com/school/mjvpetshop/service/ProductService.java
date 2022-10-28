bb8802e54a2ab52f4c12e3e19d5676e94562375a
RubensPS <rubens_p_souza@hotmail.com>
2022-10-28 16:46:17 -0300

    feat: implemented find product by id service

--- src/main/java/com/school/mjvpetshop/service/ProductService.java
+++ src/main/java/com/school/mjvpetshop/service/ProductService.java
@@ -2,6 +2,7 @@ package com.school.mjvpetshop.service;
 
 import com.school.mjvpetshop.dtoConversion.ProductDtoConversion;
 import com.school.mjvpetshop.exception.ProductAlreadyExistsException;
+import com.school.mjvpetshop.exception.ProductNotFoundException;
 import com.school.mjvpetshop.model.product.ProductEntity;
 import com.school.mjvpetshop.model.product.ProductRequest;
 import com.school.mjvpetshop.model.product.ProductResponse;
@@ -16,9 +17,15 @@ public class ProductService {
     private final ProductRepository productRepository;
 
     public ProductResponse saveNewProduct(ProductRequest request) {
-        if (productRepository.existsByName(request.getName()))
+        if (Boolean.TRUE.equals(productRepository.existsByName(request.getName())))
             throw new ProductAlreadyExistsException(String.format("A product with the name %s already exists in database.", request.getName()));
         ProductEntity entity = ProductDtoConversion.requestToEntity(request);
         return ProductDtoConversion.entityToResponse(productRepository.save(entity));
     }
+
+    public ProductResponse findProductById(Long id) {
+        ProductEntity entity = productRepository.findById(id).orElseThrow(() -> new ProductNotFoundException("A product with the provided ID doesn't exist in the database."));
+        return ProductDtoConversion.entityToResponse(entity);
+    }
+
 }
