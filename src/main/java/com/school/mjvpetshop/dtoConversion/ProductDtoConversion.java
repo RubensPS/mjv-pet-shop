0c6955609f77bf797d453daf8edbc4b1b7a95b22
RubensPS <rubens_p_souza@hotmail.com>
2022-10-28 18:24:20 -0300

    feat: implement update method

--- src/main/java/com/school/mjvpetshop/dtoConversion/ProductDtoConversion.java
+++ src/main/java/com/school/mjvpetshop/dtoConversion/ProductDtoConversion.java
@@ -4,6 +4,8 @@ import com.school.mjvpetshop.model.product.ProductEntity;
 import com.school.mjvpetshop.model.product.ProductRequest;
 import com.school.mjvpetshop.model.product.ProductResponse;
 
+import java.time.ZonedDateTime;
+
 public class ProductDtoConversion {
 
     public static ProductEntity requestToEntity(ProductRequest request) {
@@ -22,6 +24,15 @@ public class ProductDtoConversion {
                 entity.getPrice(),
                 entity.getInventory(),
                 entity.getUpdateDate());
+    }
+
+    public static ProductEntity updateEntity(ProductEntity entity, ProductRequest request) {
+        entity.setName(request.getName());
+        entity.setDescription(request.getDescription());
+        entity.setPrice(request.getPrice());
+        entity.setInventory(request.getInventory());
+        entity.setUpdateDate(ZonedDateTime.now());
+        return entity;
     }
 
 }
