187e6053c853a9c7aeb99fec96580c260aabed3c
RubensPS <rubens_p_souza@hotmail.com>
2022-10-28 16:45:46 -0300

    feat: implemented find product by id endpoint

--- src/main/java/com/school/mjvpetshop/controller/ProductController.java
+++ src/main/java/com/school/mjvpetshop/controller/ProductController.java
@@ -6,10 +6,7 @@ import com.school.mjvpetshop.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/products")
@@ -22,6 +19,12 @@ public class ProductController {
     public ResponseEntity<ProductResponse> saveNewProduct(@RequestBody ProductRequest request) {
         ProductResponse response = productService.saveNewProduct(request);
         return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductResponse> findProductById(@PathVariable Long id) {
+        ProductResponse response = productService.findProductById(id);
+        return ResponseEntity.ok(response);
     }
 
 
