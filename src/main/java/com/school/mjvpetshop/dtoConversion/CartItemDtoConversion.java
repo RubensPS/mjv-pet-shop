6bbc736e9a4d3c8c2dd124ebcdbb182614eb8545
RubensPS <rubens_p_souza@hotmail.com>
2022-10-28 22:07:16 -0300

    feat: implemented cart item request and response dto conversion methods

---
+++ src/main/java/com/school/mjvpetshop/dtoConversion/CartItemDtoConversion.java
@@ -0,0 +1,18 @@
+package com.school.mjvpetshop.dtoConversion;
+
+import com.school.mjvpetshop.model.cartItem.CartItemEntity;
+import com.school.mjvpetshop.model.cartItem.CartItemRequest;
+import com.school.mjvpetshop.model.cartItem.CartItemResponse;
+import com.school.mjvpetshop.model.product.ProductEntity;
+
+public class CartItemDtoConversion {
+
+    public static CartItemEntity requestToEntity(CartItemRequest request, ProductEntity product) {
+        return new CartItemEntity(request.getCartId(), product, request.getQuantity());
+    }
+
+    public static CartItemResponse entityToResponse(CartItemEntity entity) {
+        return new CartItemResponse(entity.getId(), entity.getQuantity());
+    }
+
+}
