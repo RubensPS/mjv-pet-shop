c963f4a0bd7cd1179c34db8adc644eeb79e1c337
RubensPS <rubens_p_souza@hotmail.com>
2022-10-28 22:11:39 -0300

    refactor: changed set element type from entity to response

--- src/main/java/com/school/mjvpetshop/dtoConversion/CartDtoConversion.java
+++ src/main/java/com/school/mjvpetshop/dtoConversion/CartDtoConversion.java
@@ -2,11 +2,16 @@ package com.school.mjvpetshop.dtoConversion;
 
 import com.school.mjvpetshop.model.cart.CartEntity;
 import com.school.mjvpetshop.model.cart.CartResponse;
+import com.school.mjvpetshop.model.cartItem.CartItemResponse;
+
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class CartDtoConversion {
 
     public static CartResponse entityToResponse(CartEntity entity) {
-        return new CartResponse(entity.getId(), entity.getItems(), entity.getTotalShopValue());
+        Set<CartItemResponse> items = entity.getItems().stream().map(CartItemDtoConversion::entityToResponse).collect(Collectors.toSet());
+        return new CartResponse(entity.getId(), items, entity.getTotalShopValue());
     }
 
 }
