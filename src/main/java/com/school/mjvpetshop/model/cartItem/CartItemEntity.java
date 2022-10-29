53e282b554e0803532f8708b9bf10c486afbee6d
RubensPS <rubens_p_souza@hotmail.com>
2022-10-28 22:02:25 -0300

    refactor: added getter, setter and constructor to class

--- src/main/java/com/school/mjvpetshop/model/cartItem/CartItemEntity.java
+++ src/main/java/com/school/mjvpetshop/model/cartItem/CartItemEntity.java
@@ -1,25 +1,40 @@
 package com.school.mjvpetshop.model.cartItem;
 
 import com.school.mjvpetshop.model.product.ProductEntity;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 import java.math.BigDecimal;
 
-@Entity(name = "cart_item")
+@Entity
+@Table(name = "cart_item")
+@Setter
+@Getter
+@NoArgsConstructor
 public class CartItemEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter(AccessLevel.NONE)
     private Long id;
 
     @JoinColumn(name = "cart_id", referencedColumnName = "id")
     private Long cartId;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "product_id", referencedColumnName = "id")
-    private ProductEntity productId;
+    @OneToOne(fetch = FetchType.LAZY)
+    @MapsId
+    private ProductEntity product;
 
     @Column(name = "quantity")
     private BigDecimal quantity;
+
+    public CartItemEntity(Long cartId, ProductEntity product, BigDecimal quantity) {
+        this.cartId = cartId;
+        this.product = product;
+        this.quantity = quantity;
+    }
 
 }
