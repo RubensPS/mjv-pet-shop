f4a07899a25f182cc6f456f650f1bac5aba29f35
RubensPS <rubens_p_souza@hotmail.com>
2022-10-28 22:10:15 -0300

    refactor: compacted imports

--- src/main/java/com/school/mjvpetshop/controller/CartController.java
+++ src/main/java/com/school/mjvpetshop/controller/CartController.java
@@ -4,10 +4,7 @@ import com.school.mjvpetshop.model.cart.CartResponse;
 import com.school.mjvpetshop.service.CartService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/carts")
